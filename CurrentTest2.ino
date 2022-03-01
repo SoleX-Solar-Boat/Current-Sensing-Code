de5005340c4957f18e9808d2d7c3513778290e45
mbr206 <100526703+mbr206@users.noreply.github.com>
2022-03-01 10:56:40 +0000

    Add files via upload
    New modifications to display Vout and Ip.

--- CurrentTest2.ino
+++ CurrentTest2.ino
@@ -2,6 +2,9 @@
 //25.02.2022
 
 float sensorPin = A0; //sensor pin A0 reads analog data from current sensor (decimal val)
+float vpp = 1024/5;
+int Ipn = 200; //Primary nominal current of sensor (200 A)
+int Uc = 5; // Input voltage from Arduino to sensor
 
 void setup() {
   Serial.begin(9600); 
@@ -9,8 +12,11 @@ void setup() {
 
 void loop() {
   int Value = analogRead (sensorPin);  //variable "Value" corresponds to sensor pin readings (intager val)
+  float Vout = (Value/vpp); // convert digital reading to analogue voltage (0-5 V)
+  float Ip = (((Vout-(0.5*Uc))/1.25)*Ipn);
+
   
-  Serial.println(Value);  //display sensor reading as serial data
+  Serial.println(Ip);  //display sensor reading as serial data
   delay(500);             //delay 1/2 second
     
 }
