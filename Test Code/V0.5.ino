3a6b18e8e91794302aa4b4539ebe459c311b2a4a
Konrad Lister-Grotz <68847530+KLister37@users.noreply.github.com>
2022-03-01 11:13:13 +0000

    Update V0.5.ino

--- Test Code/V0.5.ino
+++ Test Code/V0.5.ino
@@ -12,11 +12,14 @@ void setup() {
 void loop() {
   float sensorPin = A0;    // select the input pin for the current sensor
   float A0_Value = analogRead(sensorPin);
+  
   int Max_Value = 1024;
   int Sat_Voltage = 5;
+
   float Reference = Max_Value / Sat_Voltage
   float Vout = A0_Value/Reference; //Converting measure analogue to digatal voltage value
   float Vref = 2.5; //Read as live value
+
   float Ip = (((Vout - Vref)/1.25) * 200); 
   //float Ip = Vref + ((1.25*Vout)/200); 
   //float Ip = ((200*(Vout-Vref))/1.25); //Converting Vout to Ip
